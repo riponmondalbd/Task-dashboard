35c04b57bebea0fb1a020cb333d4effe95995225
riponmondalbd <riponmondal149@gmail.com>
2026-01-27 21:41:46 +0600

    set entire side background

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 function App() {
   return (
-    <>
-      <h1>hello world</h1>
-    </>
+    <div>
+      <h1 className="primary-color">hello world</h1>
+    </div>
   );
 }
 
 export default App;
+
+// background: linear-gradient(119deg, #020618 -34.38%, #162456 54.59%, #0F172B 143.57%);
