6a3ab77ca6403950f1a19de28e609a1280a3ee48
riponmondalbd <riponmondal149@gmail.com>
2026-01-28 04:05:39 +0600

    -->call trends --->title and selection option done also responsive

--- src/components/NavContent/Dashboard/CallTrends.jsx
+++ src/components/NavContent/Dashboard/CallTrends.jsx
@@ -1,13 +1,36 @@
 const CallTrends = () => {
   return (
     <div className="rounded-2xl border border-[#2B7FFF33]/80 bg-[#0F172B80]">
-      <div className="p-[25px]">
+      {/* title section */}
+      <div className="p-6.25 md:flex space-y-3 md:space-y-0 justify-between">
         <div>
-          <h3>Call Trends - This Week</h3>
-          <p>Total: 472 calls</p>
+          <h3 className="text-[20px] font-normal text-white">
+            Call Trends - This Week
+          </h3>
+          <p className="secondary-color text-[14px] font-normal">
+            Total: 472 calls
+          </p>
         </div>
-        <div></div>
+        <div>
+          <select
+            className="text-white border border-[#2B7FFF33]/80 rounded-[10px] py-2.75 px-4.75 focus:ring-1-[#2B7FFF33]/80 w-full  md:w-35"
+            name="data"
+            id="data"
+          >
+            <option className="bg-[#121c3a]" value="This Week">
+              This Week
+            </option>
+            <option className="bg-[#121c3a] " value="This Month">
+              This Month
+            </option>
+            <option className="bg-[#121c3a]" value="This Year">
+              This Year
+            </option>
+          </select>
+        </div>
       </div>
+
+      {/* chart section */}
       <div></div>
     </div>
   );
