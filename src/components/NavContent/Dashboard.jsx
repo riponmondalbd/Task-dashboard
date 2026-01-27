01a171b1fbca7c02286d2c5b7e1fd6baf33efbb9
riponmondalbd <riponmondal149@gmail.com>
2026-01-28 01:28:06 +0600

    top navbar responsive

--- src/components/NavContent/Dashboard.jsx
+++ src/components/NavContent/Dashboard.jsx
@@ -1,6 +1,9 @@
+import TopNavbar from "../common/TopNavbar";
+
 const Dashboard = () => {
   return (
     <div>
+      <TopNavbar title={"Dashboard Overview"} />
       <h1>Dashboard Content</h1>
     </div>
   );
--- src/components/common/TopNavbar.jsx
+++ src/components/common/TopNavbar.jsx
@@ -5,14 +5,16 @@ const TopNavbar = ({ title }) => {
     <div className="flex items-center justify-between">
       {/* title */}
       <div>
-        <h2 className="text-[32px] font-normal primary-color ml-6">{title}</h2>
+        <h2 className="text-[18px] md:text-[32px] font-normal primary-color ml-6">
+          {title}
+        </h2>
       </div>
 
       {/* notification and avatar */}
-      <div className="flex gap-10">
+      <div className="flex gap-2 md:gap-10">
         <img src={assets.notificationIcon} alt="notifications icon" />
         <img
-          className="w-20 rounded-full my-2 mr-4.75"
+          className="w-12 md:w-20 rounded-full my-2 mr-4.75"
           src={assets.avatarImage}
           alt="avatar image"
         />
