199b75ab0c0efb6ea3ddb6b72c5deb2ad405598d
riponmondalbd <riponmondal149@gmail.com>
2026-01-28 16:58:08 +0600

    basic table body

--- src/components/NavContent/Appointments/Appointment.jsx
+++ src/components/NavContent/Appointments/Appointment.jsx
@@ -1,6 +1,7 @@
 import TopNavbar from "../../common/TopNavbar";
 import BookingLink from "./BookingLink";
 import Cards from "./Cards";
+import Table from "./Table";
 
 const Appointment = () => {
   return (
@@ -9,6 +10,7 @@ const Appointment = () => {
       <div className="pl-6 py-6 pr-6 xl:pr-0 space-y-5.5">
         <Cards />
         <BookingLink />
+        <Table />
       </div>
     </div>
   );
--- src/components/NavContent/Appointments/BookingLink.jsx
+++ src/components/NavContent/Appointments/BookingLink.jsx
@@ -8,7 +8,7 @@ const BookingLink = () => {
         <p className="text-white text-[16px] font-normal bg-[#0A0A0F80] py-3 px-4 rounded-[14px] md:flex-1">
           https://techstore.com/book?id=store123
         </p>
-        <div className="flex items-center justify-center md:justify-around gap-4 bg-gradient-to-b from-[#1E2A5F] to-[#141B44] shadow-[0_1px_4px_rgba(255,255,255,0.3),0_0_15px_rgba(0,123,255,0.5),inset_0_2px_15px_rgba(255,255,255,0.5)] shadow-white-2xl py-0.5 px-2 rounded-xl cursor-pointer">
+        <div className="flex items-center justify-center md:justify-around gap-4 bg-gradient-to-b from-[#1E2A5F] to-[#141B44] shadow-[0_1px_4px_rgba(255,255,255,0.3),0_0_15px_rgba(0,123,255,0.5),inset_0_2px_15px_rgba(255,255,255,0.5)] shadow-white-2xl py-0.5 px-2 rounded-xl cursor-pointer mt-3 md:mt-0">
           <img className="w-8 h-8" src={assets.copyIcon} alt="copy icon" />
           <p className="text-white text-[15px] font-medium">Copy Link</p>
         </div>
